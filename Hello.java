6680ec7b929a656b5f306a55d6b1337bb9862d5d
Yohannes <yhailu006@gmail.com>
2024-09-03 20:34:45 -0500

    Added name line and creation detail comments

---
+++ Hello.java
@@ -0,0 +1,14 @@
+    /**
+     * Yohannes Hailu
+     * 2024/09/03
+     * Java project
+     */
+
+public class Hello
+{
+    public static void main(String[] args) {
+        System.out.println("Yohannes Hailu");
+        System.out.println("Computer Science and Math");
+        
+    }
+}
